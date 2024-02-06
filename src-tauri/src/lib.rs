a2f5db75b02e3962d50780e48c270d21f239c082
Shikano Hakukaze <hakukano@flp-web.org>
2024-02-06 15:35:17 -0800

    wip

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,17 +4,16 @@ extern crate rust_i18n;
 use tauri::{Manager, State};
 
 mod models;
-mod state;
 
 i18n!(fallback = "en_US");
 
 #[tauri::command]
-fn greet(name: &str, app_state: State<state::AppState>) -> String {
+fn greet(name: &str, models: State<models::Models>) -> String {
     format!(
         "{}: {}, {}",
         t!("ui.app_name"),
         name,
-        app_state.app_config.read().locale()
+        models.app_config.read().locale()
     )
 }
 
@@ -22,10 +21,10 @@ fn greet(name: &str, app_state: State<state::AppState>) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .manage(state::AppState::new())
+        .manage(models::Models::new())
         .setup(|app| {
             models::app_config::initialize(
-                &mut app.state::<state::AppState>().app_config.write(),
+                &mut app.state::<models::Models>().app_config.write(),
                 app,
             );
             Ok(())
--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -1,1 +1,15 @@
+use parking_lot::RwLock;
+
 pub mod app_config;
+
+pub struct Models {
+    pub app_config: RwLock<Box<dyn app_config::AppConfig>>,
+}
+
+impl Models {
+    pub fn new() -> Self {
+        Self {
+            app_config: RwLock::new(app_config::instantiate()),
+        }
+    }
+}
