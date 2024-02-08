4e3cf9a7034e2a35fd5b30ef1740f4e12f81321f
Shikano Hakukaze <hakukano@flp-web.org>
2024-02-07 23:01:48 -0800

    added bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,3 @@
-import appConfig from "./services/api/app_config.ts";
+import "./scss/styles.scss";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    const resp = await appConfig.index();
-    greetMsgEl.textContent = resp.body.locale;
-  }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet().catch((err) => console.log(err));
-  });
-});
+window.addEventListener("DOMContentLoaded", () => {});
