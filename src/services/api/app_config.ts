27a580bb8bbe26849fa5ba480586b778edf9070c
Shikano Hakukaze <hakukano@flp-web.org>
2024-02-07 22:43:18 -0800

    wip

--- src/services/api/app_config.ts
+++ src/services/api/app_config.ts
@@ -3,13 +3,22 @@ import TauriCommand from "./app_config/tauri_command.ts";
 
 export const basePath = ["app_config"];
 
-export interface IndexBody {
+export interface AppConfigImmutable {}
+
+export interface AppConfigMutable {
   locale: string;
   root_path: string | null;
 }
 
+export interface AppConfigBrief extends AppConfigImmutable, AppConfigMutable {}
+
+export interface AppConfigDetails
+  extends AppConfigImmutable,
+    AppConfigMutable {}
+
 export interface AppConfig {
-  index(): Promise<Response<IndexBody>>;
+  index(): Promise<Response<AppConfigBrief>>;
+  update(app_config: AppConfigMutable): Promise<Response<void>>;
 }
 
 const appConfig =
--- src/services/api/app_config/tauri_command.ts
+++ src/services/api/app_config/tauri_command.ts
@@ -1,8 +1,17 @@
 import { Response, send_tauri_command } from "../../api.ts";
-import { AppConfig, basePath, IndexBody } from "../app_config.ts";
+import {
+  AppConfig,
+  basePath,
+  AppConfigDetails,
+  AppConfigMutable,
+} from "../app_config.ts";
 
 export default class TauriCommand implements AppConfig {
-  async index(): Promise<Response<IndexBody>> {
+  async index(): Promise<Response<AppConfigDetails>> {
     return await send_tauri_command(basePath, "GET", {});
+  }
+
+  async update(app_config: AppConfigMutable): Promise<Response<void>> {
+    return await send_tauri_command(basePath, "PUT", app_config);
   }
 }
