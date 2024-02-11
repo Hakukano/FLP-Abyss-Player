1a9c5888786513559765ce8eeb21eb71daee606e
Shikano Hakukaze <hakukano@flp-web.org>
2024-02-11 11:16:36 -0800

    yeee

--- src-tauri/src/models/playlist.rs
+++ src-tauri/src/models/playlist.rs
@@ -1,34 +1,86 @@
+use anyhow::{anyhow, Result};
 use chrono::{DateTime, Utc};
 use serde::Serialize;
+use std::cmp::Ordering;
+
+#[derive(Clone, Copy)]
+enum MetaCmpBy {
+    Path,
+    CreateAt,
+    UpdateAt,
+}
+
+impl MetaCmpBy {
+    fn parse(s: impl AsRef<str>) -> Result<Self> {
+        match s.as_ref() {
+            "path" => Ok(MetaCmpBy::Path),
+            "created_at" => Ok(MetaCmpBy::CreateAt),
+            "updated_at" => Ok(MetaCmpBy::UpdateAt),
+            _ => Err(anyhow!("Invalid MetaCmpBy")),
+        }
+    }
+}
 
 #[derive(Serialize)]
-struct Entry {
+struct Meta {
     path: String,
+    created_at: DateTime<Utc>,
     updated_at: DateTime<Utc>,
 }
 
+impl Meta {
+    fn cmp_by(&self, other: &Meta, by: MetaCmpBy, ascend: bool) -> Ordering {
+        match by {
+            MetaCmpBy::Path => {
+                if ascend {
+                    self.path.cmp(&other.path)
+                } else {
+                    other.path.cmp(&self.path)
+                }
+            }
+            MetaCmpBy::CreateAt => {
+                if ascend {
+                    self.created_at.cmp(&other.created_at)
+                } else {
+                    other.created_at.cmp(&self.created_at)
+                }
+            }
+            MetaCmpBy::UpdateAt => {
+                if ascend {
+                    self.updated_at.cmp(&other.updated_at)
+                } else {
+                    other.updated_at.cmp(&self.updated_at)
+                }
+            }
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct Entry {
+    meta: Meta,
+}
+
 impl Entry {
-    fn new(path: String, updated_at: DateTime<Utc>) -> Self {
-        Self { path, updated_at }
+    fn new(meta: Meta) -> Self {
+        Self { meta }
     }
 
     fn matches_group(&self, group: impl AsRef<str>) -> bool {
-        self.path.starts_with(group.as_ref())
+        self.meta.path.starts_with(group.as_ref())
     }
 }
 
 #[derive(Serialize)]
 struct EntryGroup {
-    path: String,
-    updated_at: DateTime<Utc>,
+    meta: Meta,
     entries: Vec<Entry>,
 }
 
 impl EntryGroup {
-    fn new(path: String, updated_at: DateTime<Utc>) -> Self {
+    fn new(meta: Meta) -> Self {
         Self {
-            path,
-            updated_at,
+            meta,
             entries: Vec::new(),
         }
     }
@@ -36,7 +88,7 @@ impl EntryGroup {
     fn take_matched_entries(&mut self, entries: Vec<Entry>) -> Vec<Entry> {
         let mut remainders = Vec::new();
         for entry in entries.into_iter() {
-            if entry.matches_group(self.path.as_str()) {
+            if entry.matches_group(self.meta.path.as_str()) {
                 self.entries.push(entry);
             } else {
                 remainders.push(entry);
@@ -45,24 +97,9 @@ impl EntryGroup {
         remainders
     }
 
-    fn sort_by_name(&mut self, ascend: bool) {
-        self.entries.sort_by(|a, b| {
-            if ascend {
-                a.path.cmp(&b.path)
-            } else {
-                b.path.cmp(&a.path)
-            }
-        })
-    }
-
-    fn sort_by_time(&mut self, ascend: bool) {
-        self.entries.sort_by(|a, b| {
-            if ascend {
-                a.updated_at.cmp(&b.updated_at)
-            } else {
-                b.updated_at.cmp(&a.updated_at)
-            }
-        })
+    fn sort_by(&mut self, by: MetaCmpBy, ascend: bool) {
+        self.entries
+            .sort_by(|a, b| a.meta.cmp_by(&b.meta, by, ascend))
     }
 }
 
@@ -70,43 +107,20 @@ impl EntryGroup {
 struct EntryGroups(Vec<EntryGroup>);
 
 impl EntryGroups {
-    fn create_groups(&mut self, groups: Vec<(String, DateTime<Utc>)>) {
-        self.0.append(
-            &mut groups
-                .into_iter()
-                .map(|group| EntryGroup::new(group.0, group.1))
-                .collect(),
-        );
+    fn create_groups(&mut self, metas: Vec<Meta>) {
+        self.0
+            .append(&mut metas.into_iter().map(EntryGroup::new).collect());
     }
 
-    fn create_entries(&mut self, entries: Vec<(String, DateTime<Utc>)>) {
-        let mut entries = entries
-            .into_iter()
-            .map(|entry| Entry::new(entry.0, entry.1))
-            .collect::<Vec<_>>();
+    fn create_entries(&mut self, metas: Vec<Meta>) {
+        let mut entries = metas.into_iter().map(Entry::new).collect::<Vec<_>>();
         for entry_group in self.0.iter_mut() {
             entries = entry_group.take_matched_entries(entries);
         }
     }
 
-    fn sort_by_name(&mut self, ascend: bool) {
-        self.0.sort_by(|a, b| {
-            if ascend {
-                a.path.cmp(&b.path)
-            } else {
-                b.path.cmp(&a.path)
-            }
-        })
-    }
-
-    fn sort_by_time(&mut self, ascend: bool) {
-        self.0.sort_by(|a, b| {
-            if ascend {
-                a.updated_at.cmp(&b.updated_at)
-            } else {
-                b.updated_at.cmp(&a.updated_at)
-            }
-        })
+    fn sort_by(&mut self, by: MetaCmpBy, ascend: bool) {
+        self.0.sort_by(|a, b| a.meta.cmp_by(&b.meta, by, ascend))
     }
 }
 
