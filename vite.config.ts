211a119f15728a7e524bd4ebe8710d60acfc96fd
Shikano Hakukaze <hakukano@flp-web.org>
2024-03-16 22:30:31 -0700

    more cleanups

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig(async () => ({
       : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+      ignored: ["**/src-tauri/**", "**/src-derive/**"],
     },
   },
 }));
