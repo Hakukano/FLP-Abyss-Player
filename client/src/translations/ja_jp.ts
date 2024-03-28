c54faba64cca07deee5011466a6d892b3e69b205
Shikano Hakukaze <hakukano@flp-web.org>
2024-03-27 18:50:02 -0700

    added ja-jp

--- client/src/translations/ja_jp.ts
+++ client/src/translations/ja_jp.ts
@@ -9,6 +9,51 @@ export default {
     close: "閉じる",
     submit: "送信",
   },
+  menu: {
+    title: "メニュー",
+    change_playlist: "プレイリストへ",
+  },
+  sort: {
+    title: "並び替え",
+    by: {
+      label: "基準",
+      default: "デフォルト",
+      path: "パス",
+      created_at: "新規日時",
+      updated_at: "更新日時",
+    },
+    ascend: {
+      label: "上昇",
+    },
+  },
+  list: {
+    pagination: {
+      go_to: "ページへ",
+    },
+  },
+  scan: {
+    title: "メディアを探す",
+    errors: {
+      root_not_set: "ルートパスを選んでください",
+      allowed_mimes_not_set: "MIMEタイプを一つ以上選んでください",
+    },
+    root_path: {
+      label: "ルートパス",
+    },
+    allowed_mimes: {
+      label: "許可されたMIME",
+    },
+    ungrouped: {
+      title: "グループされてない",
+    },
+    grouped: {
+      title: "グループされた",
+      entry_count: "エンティティーの数",
+    },
+    scan: "探す",
+    back: "戻る",
+    submit: "送信する",
+  },
   app_config: {
     title: "アプリの設定",
     locale: {
@@ -28,5 +73,26 @@ export default {
     delete: {
       confirm: "プレイリストを削除しますか？",
     },
+  },
+  group: {
+    title: "グループ",
+    path: {
+      label: "グループパス",
+    },
+    delete: {
+      confirm: "グループを削除しますか？",
+    },
+  },
+  entry: {
+    title: "エンティティー",
+    path: {
+      label: "エンティティー",
+    },
+    delete: {
+      confirm: "エンティティーを削除しますか？",
+    },
+  },
+  player: {
+    title: "再生しています",
   },
 };
